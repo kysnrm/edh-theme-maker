7ea44e04e12802cffaf09c0596b7cc4ad95397e6
kysnrm <kysn.rm@gmail.com>
2022-07-20 16:18:14 +0900

    Scryfallからランダムな伝説のカードを取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const fetchLegendary = async () => {
+    const baseUrl = "https://api.scryfall.com/cards/random";
+
+    const randomLegendaryParams = new URLSearchParams({
+      q: "t:legend",
+    }).toString();
+    const randomLegendaryUrl = `${baseUrl}?${randomLegendaryParams}`;
+
+    await fetch(randomLegendaryUrl, { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => fetchLegendary()}>お題を見る</button>
     </div>
   );
-}
+};
 
 export default App;
