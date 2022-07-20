1c520bab124c6e0976d9a9812ad5f2359b513722
kysnrm <kysn.rm@gmail.com>
2022-07-20 16:29:12 +0900

    取得した情報をもとに名前と画像を表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
+interface Card {
+  name: string;
+  img: string;
+}
+
 const App = () => {
+  const [commander, setCommander] = useState<Card | null>();
+
   const fetchLegendary = async () => {
     const baseUrl = "https://api.scryfall.com/cards/random";
 
@@ -12,11 +20,21 @@ const App = () => {
 
     await fetch(randomLegendaryUrl, { method: "GET" })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        const name = data.name;
+        const img = data.image_uris.normal;
+        setCommander({ name, img });
+      });
   };
   return (
     <div className="App">
       <button onClick={() => fetchLegendary()}>お題を見る</button>
+      {commander && (
+        <div>
+          <div>{commander.name}</div>
+          <img src={commander.img} alt="" />
+        </div>
+      )}
     </div>
   );
 };
