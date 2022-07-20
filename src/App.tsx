bfba7a89dbded95ccb20598349840eb4668a7246
kysnrm <kysn.rm@gmail.com>
2022-07-20 16:39:40 +0900

    レスポンスがクリーチャーでない場合はもう一度リクエストする

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ interface Card {
 const App = () => {
   const [commander, setCommander] = useState<Card | null>();
 
-  const fetchLegendary = async () => {
+  const fetchCommander = async () => {
     const baseUrl = "https://api.scryfall.com/cards/random";
 
     const randomLegendaryParams = new URLSearchParams({
@@ -18,17 +18,26 @@ const App = () => {
     }).toString();
     const randomLegendaryUrl = `${baseUrl}?${randomLegendaryParams}`;
 
-    await fetch(randomLegendaryUrl, { method: "GET" })
-      .then((res) => res.json())
-      .then((data) => {
-        const name = data.name;
-        const img = data.image_uris.normal;
-        setCommander({ name, img });
-      });
+    const fetchRandomLegendary = async () => {
+      await fetch(randomLegendaryUrl, { method: "GET" })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data.type_line);
+          if (!data.type_line.includes("Creature")) {
+            fetchRandomLegendary();
+            return;
+          }
+          const name = data.name;
+          const img = data.image_uris.normal;
+          setCommander({ name, img });
+        });
+    };
+
+    await fetchRandomLegendary();
   };
   return (
     <div className="App">
-      <button onClick={() => fetchLegendary()}>お題を見る</button>
+      <button onClick={() => fetchCommander()}>お題を見る</button>
       {commander && (
         <div>
           <div>{commander.name}</div>
