4c6db9c7a2ccb2d4cc9f4e2b51d2b33ecdf00df4
kysnrm <kysn.rm@gmail.com>
2022-07-21 17:31:37 +0900

    背景を取得

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ interface Card {
   img: string;
 }
 
+type Mode = "partner" | "friend" | "bg";
+
 const App = () => {
   const [commander, setCommander] = useState<Card | null>();
   const [partner, setPartner] = useState<Card | null>();
@@ -26,30 +28,46 @@ const App = () => {
         .then((data) => {
           const name = data.name;
           const img = data.image_uris.normal;
+
+          setCommander({ name, img });
+
           if (data.keywords.includes("Partner with")) {
             const partnerName = data.oracle_text.match(
               /(?<=Partner\swith\s)(.+?)(\s\(|\n)/
             )[1];
             fetchPartnerWith(partnerName);
+            return;
           } else if (data.keywords.includes("Partner")) {
-            fetchPartner(name);
+            fetchPartner(name, "partner");
+            return;
+          } else if (data.oracle_text.includes("Friends forever")) {
+            fetchPartner(name, "friend");
+          } else if (data.oracle_text.includes("Choose a Background")) {
+            fetchPartner(name, "bg");
           } else setPartner(null);
-          if (data.oracle_text.includes("Friends forever")) {
-            fetchPartner(name, true);
-          }
-          setCommander({ name, img });
         });
     };
 
-    const randomPartnerParams = (name: string, isFriend: boolean = false) =>
-      new URLSearchParams({
-        q: `is:commander f:commander ${
-          isFriend ? "o:'Friends forever'" : "keyword:partner "
-        }-keyword:'partner with' -'${name}'`,
+    const randomPartnerParams = (name: string, mode: Mode) => {
+      const condition = (mode: Mode) => {
+        switch (mode) {
+          case "partner":
+            return "keyword:partner";
+          case "friend":
+            return "o:'Friends forever'";
+          case "bg":
+            return "type:background";
+        }
+      };
+      return new URLSearchParams({
+        q: `is:commander f:commander ${condition(
+          mode
+        )} -keyword:"partner with" -"${name}"`,
       }).toString();
+    };
 
-    const fetchPartner = async (name: string, isFriend: boolean = false) => {
-      await fetch(`${baseUrl}random?${randomPartnerParams(name, isFriend)}`, {
+    const fetchPartner = async (name: string, mode: Mode) => {
+      await fetch(`${baseUrl}random?${randomPartnerParams(name, mode)}`, {
         method: "GET",
       })
         .then((res) => res.json())
