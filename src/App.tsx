16dd688ab47562959ebd56827a7e2df1136bae96
kysnrm <kysn.rm@gmail.com>
2022-07-21 16:47:47 +0900

    共闘相手を取得

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,60 @@ interface Card {
 
 const App = () => {
   const [commander, setCommander] = useState<Card | null>();
+  const [partner, setPartner] = useState<Card | null>();
 
   const fetchCommander = async () => {
-    const baseUrl = "https://api.scryfall.com/cards/random";
+    const baseUrl = "https://api.scryfall.com/cards/";
 
     const randomLegendaryParams = new URLSearchParams({
       q: "is:commander f:commander",
     }).toString();
-    const randomLegendaryUrl = `${baseUrl}?${randomLegendaryParams}`;
 
     const fetchRandomLegendary = async () => {
-      await fetch(randomLegendaryUrl, { method: "GET" })
+      await fetch(`${baseUrl}random?${randomLegendaryParams}`, {
+        method: "GET",
+      })
         .then((res) => res.json())
         .then((data) => {
           const name = data.name;
           const img = data.image_uris.normal;
+          if (data.keywords.includes("Partner with")) {
+            const partnerName = data.oracle_text.match(
+              /(?<=Partner\swith\s)(.+?)(\s\(|\n)/
+            )[1];
+            console.log(partnerName);
+            fetchPartnerWith(partnerName);
+          } else if (data.keywords.includes("Partner")) {
+            fetchPartner();
+          } else setPartner(null);
           setCommander({ name, img });
+        });
+    };
+
+    const randomPartnerParams = new URLSearchParams({
+      q: "is:commander f:commander keyword:partner -keyword:'partner with'",
+    }).toString();
+
+    const fetchPartner = async () => {
+      await fetch(`${baseUrl}random?${randomPartnerParams}`, { method: "GET" })
+        .then((res) => res.json())
+        .then((data) => {
+          const name = data.name;
+          const img = data.image_uris.normal;
+          setPartner({ name, img });
+        });
+    };
+
+    const fetchPartnerWith = async (name: string) => {
+      const partnerParams = new URLSearchParams({
+        exact: name,
+      }).toString();
+      await fetch(`${baseUrl}named?${partnerParams}`, { method: "GET" })
+        .then((res) => res.json())
+        .then((data) => {
+          const name = data.name;
+          const img = data.image_uris.normal;
+          setPartner({ name, img });
         });
     };
 
@@ -33,12 +71,20 @@ const App = () => {
   return (
     <div className="App">
       <button onClick={() => fetchCommander()}>お題を見る</button>
-      {commander && (
-        <div>
-          <div>{commander.name}</div>
-          <img src={commander.img} alt="" />
-        </div>
-      )}
+      <div style={{ display: "flex" }}>
+        {commander && (
+          <div>
+            <div>{commander.name}</div>
+            <img src={commander.img} alt="" />
+          </div>
+        )}
+        {partner && (
+          <div>
+            <div>{partner.name}</div>
+            <img src={partner.img} alt="" />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
